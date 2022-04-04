558cbf80765be9306a8f9268d130373bc949fb6d
hamzakhalidhk <hkhalid@andrew.cmu.edu>
2022-04-04 06:28:31 -0400

    add downscaling functionality

--- image-downscaling.py
+++ image-downscaling.py
@@ -7,6 +7,7 @@ Created on Mon Apr  4 06:19:37 2022
 """
 
 import cv2
+import numpy as np
 
 file_path  = "/Users/hamzakhalid/Desktop/junebrain/simple-image-downscaling/"
 file_name = "uneven.jpg"
@@ -14,3 +15,12 @@ image_path = file_path + file_name
 
 # Reading an image in grayscale
 gray_scaled_img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+
+# Converting image to numpy array
+downscaled_img = np.asarray(gray_scaled_img)
+print("Resolution before downscaling: ", downscaled_img.shape)
+
+# Downscaling by taking every 4th pixel in both directions
+# Numpy handles all the edge cases under the hood
+downscaled_img = downscaled_img[::4,::4]
+print("Resolution after downscaling: ", downscaled_img.shape)
